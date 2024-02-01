fd2de491f2ec121882169b145d852f221364fd36
hyunji-ok <hyunjiok10@gmail.com>
2024-02-01 09:44:04 +0900

    [feat] 완주하지 못한 선수 테스트 방식 변경

---
+++ src/programmers/AthletesWhoDidNotFinishTheRace/solution.js
@@ -0,0 +1,17 @@
+module.exports = {
+    solution : (participant, completion) => {
+        let hash = {}
+        participant.forEach((par) => {
+            hash[par] = hash[par] ? hash[par] +1 : 1
+        })
+        completion.forEach((par) => {
+            hash[par] = hash[par] -1
+        })
+
+        for(let key in hash) {
+            if(hash[key] >= 1) {
+                return key
+            }
+        }
+    }
+}
---
+++ src/programmers/AthletesWhoDidNotFinishTheRace/test.js
@@ -0,0 +1,15 @@
+const { solution } = require('./solution')
+
+describe('완주하지 못한 선수 테스트', () => {
+    test('case1', () => {
+        expect(solution(['leo', 'kiki', 'eden'], ['eden', 'kiki'])).toEqual('leo');
+    });
+
+    test('case2', () => {
+        expect(solution(['marina', 'josipa', 'nikola', 'vinko', 'filipa'], ['josipa', 'filipa', 'marina', 'nikola'])).toEqual('vinko');
+    });
+
+    test('case3', () => {
+        expect(solution(['mislav', 'stanko', 'mislav', 'ana'], ['stanko', 'ana', 'mislav'])).toEqual('mislav');
+    });
+});
